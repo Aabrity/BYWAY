3e6bac7858c029cb7c7112a70b1fd62793e01e8a
Aabrity <aabrityd@gmail.com>
2023-12-11 11:22:02 +0545

    fixing code structire

--- ContactUs/contactus/src/Components/contactus/custom.d.ts
+++
@@ -1,8 +0,0 @@
-// custom.d.ts
-
-// Declare modules for specific file types
-declare module '*.png' {
-    const value: any;
-    export default value;
-  }
-  
--- ContactUs/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-import Contactus from '@/app/components/Contactus'
-
-export default function Home() {
-  return (
-    <><Contactus></Contactus> </>
-    // <h1>hello</h1>
-  )
-}
