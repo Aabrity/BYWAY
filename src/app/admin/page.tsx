15e4103a97f9120d18b9d8d01f029ab6578ce2ac
HasEEnaEEby <rkc697418@gmail.com>
2023-12-17 00:07:49 +0545

    almost done blog dummy admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,38 +1,69 @@
-
 import React from 'react';
 
-const AdminPage: React.FC = () => {
+const AdminPage = () => {
+  // Stateless variable to track whether to show the add blog container
+  const isAddBlogOpen = true; // Set this to true or false based on your logic
+
   return (
     <div className="flex h-screen bg-gray-100">
       {/* Sidebar */}
       <div className="w-1/5 bg-gray-800 p-12 text-white">
-        {/* Admin Dashboard Section */}
-        <div className="mb-12">
-          <h1 className="text-2xl font-semibold mb-10 p-10">Admin Dashboard</h1>
-        </div>
-
-        {/* Navigation Links */}
+        <h1 className="text-2xl font-semibold mb-10 p-10">Admin Dashboard</h1>
         <ul>
-          {/* Add Blog Post Section */}
           <li className="mb-4">
-            <a href="#add-blog" className="text-gray-300 hover:text-white p-4 px-6 block">
+            <a href="#add-blog" className="text-gray-300 hover:text-white p-10 py-4 px-6 block">
               Add Blog Post
             </a>
           </li>
-
-          {/* Manage Packages Section */}
           <li className='mb-2'>
-            <a href="#manage-packages" className="text-gray-300 hover:text-white p-4 px-6 block">
+            <a href="#manage-packages" className="text-gray-300 hover:text-white py-4 px-6 block">
               Manage Packages
             </a>
           </li>
         </ul>
       </div>
 
-      {/* Content Area */}
-      {/* ... */}
+       {/* Add Blog Container */}
+       {isAddBlogOpen && (
+        <div className="flex-grow bg-778C49 p-12 mt-16">
+          <div className="bg-green-200 p-6 rounded-lg h-full w-full">
+            <h2 className="text-2xl font-semibold mb-4">Add Blog Post</h2>
+            {/* Text-box example */}
+            <div className="mb-4">
+              <label htmlFor="blogContent" className="block text-sm font-medium text-gray-600">
+                Blog Content
+              </label>
+              <textarea
+                id="blogContent"
+                name="blogContent"
+                rows= '10'
+                className="mt-1 p-2 w-full border rounded-md"
+              ></textarea>
+            </div>
+            {/* Button example */}
+            <button
+              type="button"
+              className="bg-blue-500 text-white px-4 py-2 rounded-md"
+            >
+              Post
+            </button>
+            {/* Image Container */}
+            <div className="bg-green-500 p-4">
+              {/* Image upload button example */}
+              <input type="file" accept="image/*" className="mb-2" />
+              {/* Display uploaded image example */}
+              <img
+                src="https://placekitten.com/200/200" // Replace with the actual image source
+                alt="Uploaded"
+                className="max-w-90% h-auto"
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
+
 
 export default AdminPage;
