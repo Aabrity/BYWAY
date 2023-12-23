be43b69bbb56a170346e74900fae6bcd60691849
HasEEnaEEby <rkc697418@gmail.com>
2023-12-23 14:42:25 +0545

    Blogs backend completed

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,8 +1,16 @@
 "use client"
+// Import required modules
 import React, { useState } from 'react';
 import axios from 'axios';
 
-const AdminPage: React.FC = () => {
+// Check if the document object is available (client-side)
+const isBrowser = typeof window !== 'undefined';
+const ReactQuill = isBrowser ? require('react-quill') : undefined;
+
+import 'react-quill/dist/quill.snow.css';
+import './style.css';
+
+function AdminPage() {
   const [blogContent, setBlogContent] = useState('');
   const [imageFile, setImageFile] = useState<File | null>(null);
 
@@ -10,25 +18,27 @@ const AdminPage: React.FC = () => {
     try {
       const formData = new FormData();
       formData.append('content', blogContent);
-  
+
       if (imageFile) {
         formData.append('image', imageFile);
       }
-  
-      await axios.post('http://localhost:3000/api/storeContent', formData);
-  
+
+      await axios.post('http://localhost:3001/api/post-blog', formData);
+
       console.log('Content posted successfully');
     } catch (error) {
       console.error('Error posting content:', error);
     }
   };
 
-
   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
       setImageFile(e.target.files[0]);
     }
   };
+
+  // Check if ReactQuill is defined before rendering
+  if (!ReactQuill) return null;
 
   return (
     <div className="flex h-screen bg-gray-100">
@@ -42,20 +52,18 @@ const AdminPage: React.FC = () => {
         <div className="bg-green-200 p-6 rounded-lg h-full w-full">
           <h2 className="text-2xl font-semibold mb-4">Add Blog Post</h2>
 
-          <div className="mb-4">
+          <div className="editorContainer">
             <label htmlFor="blogContent" className="block text-sm font-medium text-gray-600">
               Blog Content
             </label>
-            <textarea
-              id="blogContent"
-              name="blogContent"
-              rows={10}
-              className="mt-1 p-2 w-full border rounded-md"
-              value={blogContent}
-              onChange={(e) => setBlogContent(e.target.value)}
-            ></textarea>
+            
+            {/* Render ReactQuill only on the client-side */}
+            {ReactQuill && (
+              <ReactQuill className="editor" theme="snow" value={blogContent} onChange={setBlogContent} />
+            )}
           </div>
 
+          {/* Displaying the image */}
           <input
             type="file"
             accept="image/*"
@@ -82,7 +90,6 @@ const AdminPage: React.FC = () => {
       </div>
     </div>
   );
-};
-
+}
 
 export default AdminPage;
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -23,6 +23,10 @@ function Blogs() {
   const toggleFullContent = () => {
     setShowFullContent(!showFullContent);
   };
+
+  const closeFullContent = () => {
+    setShowFullContent(false);
+  };
   return (
     <>
     <div style={{ height: "60vh", position: "relative" }}>
@@ -111,139 +115,147 @@ function Blogs() {
 
           {/* COntainer number 2 */}
 
-          <div>
-      <div style={{ flexShrink: 0, width: '500px', marginRight: '10px', boxShadow: '2px 4px 8px rgba(0, 0, 0, 0.6)', borderRadius: '10px', padding: '20px', overflow: 'hidden' }}>
-        <div style={{ height: '500px', position: 'relative' }}>
-          <Image src="/images/coverimage.jpg" alt="CoverImage" width={600} height={150} />
-          <b style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>The ABC trek</b>
-          <Paragraph />
-        </div>
-      </div>
-
-      {showFullContent && (
-        <div style={{ position: 'fixed', top: '0', left: '0', width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', zIndex: '100', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <div style={{ background: '#fff', padding: '20px', borderRadius: '10px', width: '80%', height: '80%', overflow: 'auto' }}>
-            {/* Full content goes here */}
-            <Image src="/images/coverimage.jpg" alt="CoverImage" width={600} height={150} />
-            <b>The ABC trek</b>
-            <Paragraph />
+  
+        {/* Container number 1 */}
+        <div style={{ flexShrink: 0, width: '500px', marginRight: '10px', boxShadow: '2px 4px 8px rgba(0, 0, 0, 0.6)', borderRadius: '10px', padding: '20px', overflow: 'hidden' }}>
+          <div style={{ height: '500px', position: 'relative' }}>
+            <img src="/assets/pokhera thulokot.webp" alt="Pokhera" width={600} height={150} />
+            <b style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>The ABC trek</b>
+            {/* Display only a portion of the content initially */}
+            {showFullContent ? (
+              <>
+              
+                <p>{/* Your full content goes here */}</p>
+                <button onClick={closeFullContent}>Close</button>
+              </>
+            ) : (
+              <>
+                <p>
+                  This trek is also known as the Annapurna Sanctuary trek; Trail Follows winding trails along forested slopes and treks alongside rushing rivers of surpassing beauty as we approach Annapurna Base Camp on this uniquely accessible six-day itinerary.
+      
+      It's an incredible combination of diverse cultures and nature. This trip is more than just a trek that we will never forget.
+      
+      Annapurna Base Camp Trekking ABC is one of the most popular hikes in the world.
+                </p>
+                <button onClick={toggleFullContent}>Read More</button>
+              </>
+            )}
           </div>
         </div>
-      )}
-      <button onClick={toggleFullContent}>Read More</button>
-    </div>
 
             {/* Container number 3 */}
-            <div>
-      <div style={{ flexShrink: 0, width: '500px', marginRight: '10px', boxShadow: '2px 4px 8px rgba(0, 0, 0, 0.6)', borderRadius: '10px', padding: '20px', overflow: 'hidden' }}>
-        <div style={{ height: '500px', position: 'relative' }}>
-          <Image src="/images/Mountains 1.jpg" alt="Mountaines" width={600} height={150} />
-          <b style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>The ABC trek</b>
-          <Paragraph />
-        </div>
-      </div>
-
-      {showFullContent && (
-        <div style={{ position: 'fixed', top: '0', left: '0', width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', zIndex: '100', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <div style={{ background: '#fff', padding: '20px', borderRadius: '10px', width: '80%', height: '80%', overflow: 'auto' }}>
-            {/* Full content goes here */}
-            <Image src="/images/Mountains 1.jpg" alt="Mountains" width={600} height={150} />
-            <b>The ABC trek</b>
-            <Paragraph />
+            <div style={{ flexShrink: 0, width: '500px', marginRight: '10px', boxShadow: '2px 4px 8px rgba(0, 0, 0, 0.6)', borderRadius: '10px', padding: '20px', overflow: 'hidden' }}>
+          <div style={{ height: '500px', position: 'relative' }}>
+            <img src="/images/Mountains 1.jpg" alt="Pokhera" width={600} height={150} />
+            <b style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>The ABC trek</b>
+            {/* Display only a portion of the content initially */}
+            {showFullContent ? (
+              <p> 
+             </p>
+            ) : (
+              <>
+                <p> This trek is also known as the Annapurna Sanctuary trek; Trail Follows winding trails along forested slopes and treks alongside rushing rivers of surpassing beauty as we approach Annapurna Base Camp on this uniquely accessible six-day itinerary.
+      
+      It's an incredible combination of diverse cultures and nature. This trip is more than just a trek that we will never forget.
+      
+      Annapurna Base Camp Trekking ABC is one of the most popular hikes in the world.</p>
+                <button onClick={toggleFullContent}>Read More</button>
+              </>
+            )}
           </div>
         </div>
-      )}
-      <button onClick={toggleFullContent}>Read More</button>
-    </div>
 
             {/* Container number 4 */}
 
-            <div>
-      <div style={{ flexShrink: 0, width: '500px', marginRight: '10px', boxShadow: '2px 4px 8px rgba(0, 0, 0, 0.6)', borderRadius: '10px', padding: '20px', overflow: 'hidden' }}>
-        <div style={{ height: '500px', position: 'relative' }}>
-          <Image src="/images/istockphoto-866904464-612x612.jpg" alt="Instock" width={600} height={150} />
-          <b style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>The ABC trek</b>
-          <Paragraph />
-        </div>
-      </div>
-
-      {showFullContent && (
-        <div style={{ position: 'fixed', top: '0', left: '0', width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', zIndex: '100', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <div style={{ background: '#fff', padding: '20px', borderRadius: '10px', width: '80%', height: '80%', overflow: 'auto' }}>
-            {/* Full content goes here */}
-            <Image src="/images/istockphoto-866904464-612x612.jpg" alt="Instock" width={600} height={150} />
-            <b>The ABC trek</b>
-            <Paragraph />
+            <div style={{ flexShrink: 0, width: '500px', marginRight: '10px', boxShadow: '2px 4px 8px rgba(0, 0, 0, 0.6)', borderRadius: '10px', padding: '20px', overflow: 'hidden' }}>
+          <div style={{ height: '500px', position: 'relative' }}>
+            <img src="/everest-base-camp-10.jpg" alt="Pokhera" width={600} height={150} />
+            <b style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>The ABC trek</b>
+            {/* Display only a portion of the content initially */}
+            {showFullContent ? (
+              <>
+                <button style={{ position: 'absolute', top: '10px', right: '10px' }} onClick={closeFullContent}>Close</button>
+                <p>{/* Your full content goes here */}</p>
+              </>
+            ) : (
+              <>
+                <p> This trek is also known as the Annapurna Sanctuary trek; Trail Follows winding trails along forested slopes and treks alongside rushing rivers of surpassing beauty as we approach Annapurna Base Camp on this uniquely accessible six-day itinerary.
+      
+      It's an incredible combination of diverse cultures and nature. This trip is more than just a trek that we will never forget.
+      
+      Annapurna Base Camp Trekking ABC is one of the most popular hikes in the world.</p>
+                <button onClick={toggleFullContent}>Read More</button>
+              </>
+            )}
           </div>
         </div>
-      )}
-      <button onClick={toggleFullContent}>Read More</button>
-    </div>
 
             {/* {The container 5} */}
-        <div>
-      <div style={{ flexShrink: 0, width: '500px', marginRight: '10px', boxShadow: '2px 4px 8px rgba(0, 0, 0, 0.6)', borderRadius: '10px', padding: '20px', overflow: 'hidden' }}>
-        <div style={{ height: '500px', position: 'relative' }}>
-          <Image src="/everest-base-camp-10.jpg" alt="Everest" width={600} height={150} />
-          <b style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>The ABC trek</b>
-          <Paragraph />
-        </div>
-      </div>
-
-      {showFullContent && (
-        <div style={{ position: 'fixed', top: '0', left: '0', width: '100%', height: '100%', backgroundColor: 'rgba(0, 0, 0, 0.5)', zIndex: '100', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <div style={{ background: '#fff', padding: '20px', borderRadius: '10px', width: '80%', height: '80%', overflow: 'auto' }}>
-            {/* Full content goes here */}
-            <Image src="/everest-base-camp-10.jpg" alt="Everest" width={600} height={150} />
-            <b>The ABC trek</b>
-            <Paragraph />
-          </div>
-        </div>
-      )}
-      <button onClick={toggleFullContent}>Read More</button>
-    </div>
-
-      </ScrollableContainer>
-
-      <div>
-      <h1 style={{ fontSize: '40px', margin:'30px 0', padding:'10px'}}> 
-        <b>
-          Recent Travels 
-        </b>
-      </h1>
-
-      <ScrollableContainer>
-        {/* Existing containers... */}
-
-        {/* Container for Recent Travels */}
-        <div
-          style={{
-            flexShrink: 0,
-            width: '500px',
-            marginRight: '10px',
-            boxShadow: '2px 4px 8px rgba(0, 0, 0, 0.6)',
-            borderRadius: '10px',
-            padding: '20px',
-            cursor: 'pointer',
-          }}
-          onClick={openDetailView}
-        >
-          {/* Container content */}
+            <div style={{ flexShrink: 0, width: '500px', marginRight: '10px', boxShadow: '2px 4px 8px rgba(0, 0, 0, 0.6)', borderRadius: '10px', padding: '20px', overflow: 'hidden' }}>
           <div style={{ height: '500px', position: 'relative' }}>
-            <Image src="/assets/travel-image.jpg" alt="Travel Image" width={600} height={150} />
-            <p>
-              {/* Add content here */}
-            </p>
+            <img src="/assets/pokhera thulokot.webp" alt="Pokhera" width={600} height={150} />
+            <b style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>The ABC trek</b>
+            {/* Display only a portion of the content initially */}
+            {showFullContent ? (
+              <>
+                <p>{/* Your full content goes here */}</p>
+                <button onClick={closeFullContent}>Close</button>
+              </>
+            ) : (
+              <>
+                <p> This trek is also known as the Annapurna Sanctuary trek; Trail Follows winding trails along forested slopes and treks alongside rushing rivers of surpassing beauty as we approach Annapurna Base Camp on this uniquely accessible six-day itinerary.
+      
+      It's an incredible combination of diverse cultures and nature. This trip is more than just a trek that we will never forget.
+      
+      Annapurna Base Camp Trekking ABC is one of the most popular hikes in the world.</p>
+                <button onClick={toggleFullContent}>Read More</button>
+              </>
+            )}
           </div>
         </div>
 
-        {/* Add more containers as needed */}
       </ScrollableContainer>
 
-      {/* Recent Travels Detail View */}
-      {showDetailView && <DetailView onClose={closeDetailView} />}
+    <div style={{
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100vh', // Adjust as needed
+    }}>
+  <h1 style={{ fontSize: '40px', margin: '30px 0', padding: '10px' }}>
+    <b>
+      Recent Travels
+    </b>
+  </h1>
 
+  <ScrollableContainer >
+    {/* Container for Recent Travels */}
+    <div
+      style={{
+        flexShrink: 0,
+        width: '90%', // Adjust the width as needed
+        maxWidth: '900px', // Add a maximum width if desired
+        boxShadow: '2px 4px 8px rgba(0, 0, 0, 0.6)',
+        borderRadius: '10px',
+        padding: '20px',
+        cursor: 'pointer',
+        boxSizing: 'border-box', // Add this property to include padding and border in the width
+      }}
+      onClick={openDetailView}
+    >
+      {/* Container content */}
+      <div style={{ height: '500px', position: 'relative' }}>
+        <Image src="/images/istockphoto-866904464-612x612.jpg" alt="Travel Image" width={600} height={150} />
+        <p>
+          <Paragraph/>
+        </p>
+      </div>
     </div>
+    {/* Add more containers as needed */}
+  </ScrollableContainer>
+</div>
+
     </>
   );
 }
