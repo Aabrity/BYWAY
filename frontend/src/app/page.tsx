65c413bda1ffc6541072acf102a79cfa589036c2
Rohan <roahhanchaulagain@gmail.com>
2023-12-26 22:20:26 +0545

    features final edit completed

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
         />
       </div>
       <SectionTitle
-        title="Main Features"
-        paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+        title="Our Services"
+        paragraph="Embark on extraordinary adventures with our travel agency. Discover the world's most captivating destinations, tailored to your desires. At [Your Company Name], we redefine travel, offering a seamless and enriching experience that goes beyond the ordinary. Our commitment to excellence ensures every journey is a masterpiece, filled with moments that last a lifetime."
         center
         mb='0'
       />
