70af4550687ecc7c4b9b5a999eb2b699b432f628
HasEEnaEEby <rkc697418@gmail.com>
2023-12-27 22:34:00 +0545

    The formate pull from main

---
+++ backend/contactus.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mysql from "mysql";
+
+const router = express.Router();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "rohan",
+  password: "357951",
+  database: "byway",
+});
+
+router.post("/contactus", (req, res) => {
+  const sqlInsert =
+    "INSERT INTO contacttable(email, phone,subject,address,message) VALUES (?,?,?,?,?)";
+  const Email = req.body.Email;
+  const contactnum = req.body.contactnum;
+  const subject = req.body.subject;
+  const address = req.body.address;
+  const message = req.body.message;
+
+  db.query(
+    sqlInsert,
+    [Email, contactnum, subject, address, message],
+    (err, result) => {
+      if (err) return res.json(err);
+      return res.json(Result);
+    }
+  );
+});
+
+export default router;
---
+++ backend/packages.js
@@ -0,0 +1,63 @@
+import express from "express";
+import mysql from "mysql";
+
+const router = express.Router();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "rohan",
+  password: "357951",
+  database: "byway",
+});
+
+router.post("/addpackages", (req, res) => {
+  const {
+    title,
+    location_id,
+    about,
+    guidance_language,
+    whats_included,
+    what_to_expect,
+    departure_and_return,
+    accessibility,
+    additional_info,
+  } = req.body;
+  const insertQuery =
+    "INSERT INTO packagetable (package_title, location_id, about, guidance_language, whats_included, what_to_expect, departure_and_return, accessibility, additional_info) VALUES (?)";
+  const values = [
+    title,
+    location_id,
+    about,
+    guidance_language,
+    whats_included,
+    what_to_expect,
+    departure_and_return,
+    accessibility,
+    additional_info,
+  ];
+  db.query(insertQuery, [values], (err, result) => {
+    if (err) {
+      console.log("Insertion error:", err);
+      return res.json("Insertion error");
+    }
+    return res.json({ Status: "Success" });
+  });
+});
+
+router.delete("/deletepackages/:id", (req, res) => {});
+
+router.post("/addlocations", (req, res) => {
+  const { location } = req.body;
+  const query = "INSERT INTO locationtable (location) VALUES (?)";
+
+  db.query(query, [location], (err, result) => {
+    if (err) {
+      console.error("Error executing query:", err);
+      res.status(500).json({ status: "Error" });
+    } else {
+      res.status(200).json({ status: "Success", location_id: result.insertId });
+    }
+  });
+});
+
+export default router;
