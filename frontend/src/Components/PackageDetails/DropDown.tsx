c4dfa858163d1572663bb9c3c6927575d02d12dc
Rohan <roahhanchaulagain@gmail.com>
2023-12-30 21:27:01 +0545

    reorganize after merge

---
+++ frontend/src/Components/PackageDetails/DropDown.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+
+
+const ExpandableSection = ({ buttonLabel, expandedContent }) => {
+  const [isExpanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!isExpanded);
+  };
+
+  return (
+    <div>
+    <div className="bg-white pt-4 rounded-lg ">
+    
+   
+      <button
+        className=" text-xl font-bold text-black "
+        onClick={handleExpandClick}
+      >
+        {buttonLabel}
+      </button>
+
+      {isExpanded && (
+        <div className=" ">
+         
+          {expandedContent}
+        </div>
+      )}
+       
+    </div>
+    <hr className=" border-black p-px2" />
+    </div>
+  );
+};
+
+export default ExpandableSection;
+
+
