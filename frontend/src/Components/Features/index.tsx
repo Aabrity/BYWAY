4d478b2155ce71ad5fef6e84e1b1226b153e73d0
Rohan <roahhanchaulagain@gmail.com>
2023-12-31 08:18:27 +0545

    after merge err fix

--- frontend/src/Components/Features/index.tsx
+++ frontend/src/Components/Features/index.tsx
@@ -12,7 +12,8 @@ const Features = () => {
           <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
             {features.map((feature) => (
               <FeatureCard
-                id={feature.id}
+              key={feature.id}  
+              id={feature.id}
                 title={feature.title}
                 description={feature.description}
                 icon={feature.icon}
