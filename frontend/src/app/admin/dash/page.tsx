8abc3fde0e40ec669b0eef86caf06d8a3ca819e5
Rohan <roahhanchaulagain@gmail.com>
2024-01-02 13:16:09 +0545

    pop for non logged in added

--- frontend/src/app/admin/dash/page.tsx
+++ frontend/src/app/admin/dash/page.tsx
@@ -3,9 +3,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
-
-import Sidebar from '@/Components/Sidebar/index'
+import Popup from '@/Components/Popup/index'
 
 export default function Dash() {
   const [auth, setAuth] = useState(false);
@@ -43,11 +41,7 @@ export default function Dash() {
         </div>
       ) : (
         <div>
-          <h3>{message}</h3>
-          <h3>Login now</h3>
-          <Link href="/admin" className="btn btn-primary">
-            Login
-          </Link>
+        <Popup message="You are not authenticated" buttonText="Login now" onClose={() => {  }} />
         </div>
       )}
 </Sidebar>
