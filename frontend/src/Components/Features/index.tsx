5cb2964b3d3ff01553a0ecd72757e62f9848ba7b
RohaN ChAuLAgAiN <86584369+click-boom@users.noreply.github.com>
2024-01-02 20:13:21 +0545

    Update index.tsx

--- frontend/src/Components/Features/index.tsx
+++ frontend/src/Components/Features/index.tsx
@@ -7,7 +7,6 @@ const Features = () => {
   return (
     <>
       <section id="features" className="flex py-16 md:py-20 lg:py-28 justify-center ">
-      <section id="features" className="flex py-16 md:py-20 lg:py-28 justify-center ">
         <div className="container">
           <div className="grid grid-cols-1 gap-x-8 gap-y-14 md:grid-cols-2 lg:grid-cols-3">
             {features.map((feature) => (
