6352840ccc216605f8eb6e5bd9e0a5dd6aa57b96
HasEEnaEEby <rkc697418@gmail.com>
2024-01-28 08:29:00 +0545

    we have modified the trending

--- frontend/src/Components/Blogs/TrendingBlogContainer.tsx
+++ frontend/src/Components/Blogs/TrendingBlogContainer.tsx
@@ -11,16 +11,16 @@ interface TrendingBlogContainerProps {
 
 const TrendingBlogContainer: React.FC<TrendingBlogContainerProps> = ({ title, location, date, description, imageSrc }) => {
   return (
-    <div className="w-67 p-3 mb-4 bg-white border border-gray-300 rounded-md shadow-xl">
-    <div className="mb-2" style={{ position: "relative", width: "100%", height: "460px" }}>
-    <Image
-      src={imageSrc}
-      alt={title}
-      layout="fill"
-      objectFit="cover" 
-      className="rounded-md"
-    />
-  </div>
+    <div className="w-2/3 p-3 mb-4 bg-white border border-gray-300 rounded-md shadow-xl">
+      <div className="mb-2 relative w-full h-96">
+        <Image
+          src={imageSrc}
+          alt={title}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-md"
+        />
+      </div>
       <h2 className="text-xl font-bold mb-2">{title}</h2>
       <div className="flex items-center mb-2">
         <FaMapMarkerAlt className="mr-2" />
