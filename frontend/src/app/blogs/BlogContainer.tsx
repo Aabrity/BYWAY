9a05f1460de78519bb92d558bd6ae272a3447bc3
bipasha <bipashalamsal@gmail.com>
2024-01-28 18:16:05 +0545

    changes

--- frontend/src/app/blogs/BlogContainer.tsx
+++ frontend/src/app/blogs/BlogContainer.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import Image from 'next/image';
 
 interface BlogContainerProps {
-  title: string;
-  description: string;
+  title: string | string[];
   publishedDate: string;
   imageSrc: string;
 }
 
 const BlogContainer: React.FC<BlogContainerProps> = ({
   title,
-  description,
   publishedDate,
   imageSrc,
 }) => {
   return (
     <div
       style={{
-        flexShrink: 0,
-        width: "32%",
-        height: "600px",
+        width: "100%",
+        height: "460px",
         boxShadow: "2px 2px 8px rgba(0, 0, 0, 0.6)",
         borderRadius: "10px",
         padding: "20px",
@@ -27,27 +23,58 @@ const BlogContainer: React.FC<BlogContainerProps> = ({
         transition: "transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out",
         cursor: "pointer",
         boxSizing: "border-box",
+        marginBottom: "30px",
       }}
     >
+      <div className='mt-4 , mb-5'>
+        <img
+          src={imageSrc}
+          alt={Array.isArray(title) ? title.join(' ') : title}
+          style={{ width: '367px', height: '300px' }}
+        />
+      </div>
       <div>
-        <h2
-          style={{
-            whiteSpace: "nowrap",
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-          }}
-        >
-          {title}
-        </h2>
-        <small>Date: {new Date(publishedDate).toLocaleDateString()}</small>
+        {Array.isArray(title) ? (
+          title.map((t, index) => (
+            <p
+              key={index}
+              style={{
+                whiteSpace: "nowrap",
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                fontSize: "20px",
+                fontFamily: "inherit",
+                fontWeight: "bold",
+                marginBottom: "-2px",
+             
+              }}
+            >
+              {t}
+            </p>
+          ))
+        ) : (
+          <p
+            style={{
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              fontSize: "20px",
+              fontFamily: "inherit",
+              fontWeight: "bold",
+            }}
+          >
+            {title}
+          </p>
+        )}
+        <small style={{ fontSize: "14px", fontFamily: "inherit", fontWeight: "500" }}>
+          Post date: {new Date(publishedDate).toLocaleDateString()}
+        </small>
       </div>
 
-      <div style={{ height: "500px", position: "relative" }}>
-        <Image src={imageSrc} alt={title} width={600} height={150} />
-        <p>{description}</p>
-      </div>
+      
     </div>
   );
 };
 
 export default BlogContainer;
+ 
